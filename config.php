76032c2713d2c924a2284ed65b57d41a79e063c9
Aryhermansyah <aryhermansyah29@gmail.com>
2025-04-30 21:56:27 +0700

    Add config.php 1746024987224

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'wedding_rundown');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Other configurations can be added here
+?>
